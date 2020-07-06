af4ca0a9d3b8e873c428d916fa58e1a0effff4b5
bainss <surjit.bains@gmail.com>
2020-07-06 21:50:00 +0100

    Update providers.tf

--- terraform/live/shared/eu-west-2/eks/providers.tf
+++ terraform/live/shared/eu-west-2/eks/providers.tf
@@ -37,11 +37,11 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 
 data "aws_eks_cluster" "cluster" {
-  name = aws_eks_cluster.*.id
+  name = var.eks["cluster_name"]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.*.id
+  name = var.eks["cluster_name"]
 }
 
 variable "aws" {
