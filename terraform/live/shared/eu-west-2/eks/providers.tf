37b90c01801de33536cc379d2ffb2a7f605b6e5e
bainss <surj@polarpoint.io>
2020-07-07 19:44:58 +0100

    provider reference

--- terraform/live/shared/eu-west-2/eks/providers.tf
+++ terraform/live/shared/eu-west-2/eks/providers.tf
@@ -27,11 +27,11 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 
 data "aws_eks_cluster" "cluster" {
-  name = data.aws_eks_cluster.cluster.id
+  name = aws_eks_cluster.this[0].id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = data.aws_eks_cluster.cluster.id
+  name = aws_eks_cluster.this[0].id
 }
 
 variable "aws" {
